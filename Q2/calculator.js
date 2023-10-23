699a20a90293978f240792a203540d69e13a1a98
Souravpatilgithub <148744329+Souravpatilgithub@users.noreply.github.com>
2023-10-23 12:00:28 +0530

    Initial commit

---
+++ Q2/calculator.js
@@ -0,0 +1,76 @@
+//function c_one(){
+   // var dis=document.getElementById("display").value;
+    //dis +='1';
+    //document.getElementById('display').value=dis;
+/*}
+function c_two(){
+    var dis=document.getElementById("display").value;
+    dis +='2';
+    document.getElementById('display').value=dis;
+}
+function c_three(){
+    var dis=document.getElementById("display").value;
+    dis +='3';
+    document.getElementById('display').value=dis;
+}
+function c_four(){
+    var dis=document.getElementById("display").value;
+    dis +='4';
+    document.getElementById('display').value=dis;
+}
+function c_five(){
+    var dis=document.getElementById("display").value;
+    dis +='5';
+    document.getElementById('display').value=dis;
+}
+function c_six(){
+    var dis=document.getElementById("display").value;
+    dis +='6';
+    document.getElementById('display').value=dis;
+}
+function c_seven(){
+    var dis=document.getElementById("display").value;
+    dis +='7';
+    document.getElementById('display').value=dis;
+}
+function c_eight(){
+    var dis=document.getElementById("display").value;
+    dis +='8';
+    document.getElementById('display').value=dis;
+}
+function c_nine(){
+    var dis=document.getElementById("display").value;
+    dis +='9';
+    document.getElementById('display').value=dis;
+}
+
+function c_ac(){
+    var dis=document.getElementById("display").value;
+    dis2 ='';
+    document.getElementById('display').value=dis2;
+}
+function c_de(){
+    var dis=document.getElementById("display").value;
+    dis1 =dis.slice(0,-1);
+    document.getElementById('display').value=dis1;
+}
+function c_add(){
+    var dis=document.getElementById("display").value;
+    dis +='+';
+    document.getElementById('display').value=dis;//
+}
+function c_sub(){
+    var dis=document.getElementById("display").value;
+    dis +='-';
+    document.getElementById('display').value=dis;
+}
+function c_multi(){
+    var dis=document.getElementById("display").value;
+    dis +='*';
+    document.getElementById('display').value=dis;
+}
+function c_div(){
+    var dis=document.getElementById("display").value;
+    dis +='/';
+    document.getElementById('display').value=dis;
+}*/
